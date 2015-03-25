4f33c031a24cdacea3f2f8ab305f9cb29df1749e
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-03-25 13:10:13 -0400

    Measuring more reliable exec times in SsbQueryRunner
    1. do more iterations in SsbQueryRunner
    2. avoid the cost of transfering data between spark job and  the driver.

--- src/main/scala/edu/osu/cse/fathi/spark/ssb/SsbQueryRunner.scala
+++ src/main/scala/edu/osu/cse/fathi/spark/ssb/SsbQueryRunner.scala
@@ -157,57 +157,70 @@ object SsbQueryRunner {
                     |order by d_year asc,revenue desc
                     | """.stripMargin),
 
-      ("SSBQ4.1", """
-                    |select d_year,c_nation,sum(lo_revenue-lo_supplycost) as profit
-                    |from lineorder,supplier,customer,part, ddate
-                    |where lo_custkey = c_custkey
-                    |and lo_suppkey = s_suppkey
-                    |and lo_partkey = p_partkey
-                    |and lo_orderdate = d_datekey
-                    |and c_region = 'AMERICA'
-                    |and s_region = 'AMERICA'
-                    |and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
-                    |group by d_year,c_nation
-                    |order by d_year,c_nation
-                    | """.stripMargin),
+      ("SSBQ3.4 Hand Optimized", """
+                                   |select c_city,s_city,d_year,sum(lo_revenue) as revenue
+                                   |from customer,supplier,ddate,lineorder
+                                   |where lo_custkey = c_custkey
+                                   |and lo_suppkey = s_suppkey
+                                   |and lo_orderdate = d_datekey
+                                   |and (c_city = 'UNITED KI1' or c_city = 'UNITED KI5')
+                                   |and (s_city = 'UNITED KI1' or s_city = 'UNITED KI5')
+                                   |and d_yearmonth = 'Dec1997'
+                                   |group by c_city,s_city,d_year
+                                   |order by d_year asc,revenue desc
+                                   | """.stripMargin)
 
-      ("SSBQ4.2", """
-                    |select d_year,s_nation,p_category,sum(lo_revenue-lo_supplycost) as profit
-                    |from lineorder,customer,supplier,part,ddate
-                    |where lo_custkey = c_custkey
-                    |and lo_suppkey = s_suppkey
-                    |and lo_partkey = p_partkey
-                    |and lo_orderdate = d_datekey
-                    |and c_region = 'AMERICA'
-                    |and s_region = 'AMERICA'
-                    |and (d_year = 1997 or d_year = 1998)
-                    |and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
-                    |group by d_year,s_nation, p_category
-                    |order by d_year,s_nation, p_category
-                    | """.stripMargin),
+            ("SSBQ4.1", """
+                          |select d_year,c_nation,sum(lo_revenue-lo_supplycost) as profit
+                          |from lineorder,supplier,customer,part, ddate
+                          |where lo_custkey = c_custkey
+                          |and lo_suppkey = s_suppkey
+                          |and lo_partkey = p_partkey
+                          |and lo_orderdate = d_datekey
+                          |and c_region = 'AMERICA'
+                          |and s_region = 'AMERICA'
+                          |and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
+                          |group by d_year,c_nation
+                          |order by d_year,c_nation
+                          | """.stripMargin),
 
-      ("SSBQ4.3", """
-                    |select d_year,s_city,p_brand1,sum(lo_revenue-lo_supplycost) as profit
-                    |from lineorder,supplier,customer,part,ddate
-                    |where lo_custkey = c_custkey
-                    |and lo_suppkey = s_suppkey
-                    |and lo_partkey = p_partkey
-                    |and lo_orderdate = d_datekey
-                    |and c_region = 'AMERICA'
-                    |and s_nation = 'UNITED STATES'
-                    |and (d_year = 1997 or d_year = 1998)
-                    |and p_category = 'MFGR#14'
-                    |group by d_year,s_city,p_brand1
-                    |order by d_year,s_city,p_brand1
-                    | """.stripMargin)
+            ("SSBQ4.2", """
+                          |select d_year,s_nation,p_category,sum(lo_revenue-lo_supplycost) as profit
+                          |from lineorder,customer,supplier,part,ddate
+                          |where lo_custkey = c_custkey
+                          |and lo_suppkey = s_suppkey
+                          |and lo_partkey = p_partkey
+                          |and lo_orderdate = d_datekey
+                          |and c_region = 'AMERICA'
+                          |and s_region = 'AMERICA'
+                          |and (d_year = 1997 or d_year = 1998)
+                          |and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
+                          |group by d_year,s_nation, p_category
+                          |order by d_year,s_nation, p_category
+                          | """.stripMargin),
+
+            ("SSBQ4.3", """
+                          |select d_year,s_city,p_brand1,sum(lo_revenue-lo_supplycost) as profit
+                          |from lineorder,supplier,customer,part,ddate
+                          |where lo_custkey = c_custkey
+                          |and lo_suppkey = s_suppkey
+                          |and lo_partkey = p_partkey
+                          |and lo_orderdate = d_datekey
+                          |and c_region = 'AMERICA'
+                          |and s_nation = 'UNITED STATES'
+                          |and (d_year = 1997 or d_year = 1998)
+                          |and p_category = 'MFGR#14'
+                          |group by d_year,s_city,p_brand1
+                          |order by d_year,s_city,p_brand1
+                          | """.stripMargin)
     )
 
-    val ITERATIONS = 1
-    (1 to ITERATIONS).foreach { iterationNumber =>
-      ssbQueries.foreach { case (queryName, sqlString) =>
+    val ITERATIONS = 20
+    ssbQueries.foreach { case (queryName, sqlString) =>
+      val queryResults = sqlContext.sql(sqlString)
+      (1 to ITERATIONS).foreach { iterationNumber =>
         val startTime = System.nanoTime()
-        val queryResults = sqlContext.sql(sqlString)
-        queryResults.collect()
+        queryResults.foreachPartition(partition => println("Running the partition"))
         val endTime = System.nanoTime()
         val executionTime = endTime - startTime;
 
