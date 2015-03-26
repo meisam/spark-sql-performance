a1322ed648f3c93ae76a75bf40a4e04b5540ac90
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-03-26 17:42:52 -0400

    Fixing SSB 1.2 hand optimized

--- src/main/scala/edu/osu/cse/fathi/spark/ssb/SsbQueryRunnerOnSpark.scala
+++ src/main/scala/edu/osu/cse/fathi/spark/ssb/SsbQueryRunnerOnSpark.scala
@@ -185,30 +185,40 @@ object SsbQueryRunnerOnSpark {
    * }}
    */
   val hand_opt_ssb_1_2 = (sc: SparkContext, dbDir: String) => {
-
-    val rdd000 = sc.textFile(dbDir + "/lineorder*")
-    val rdd001 = rdd000.map(line => {
+    val rddDate = sc.textFile(dbDir + "/ddate*").map(line => {
       val columns = line.split("\\|")
-      (columns(5), columns(8).toInt, columns(9).toFloat, columns(11).toInt)
-    })
-    val rdd002 = rdd001.filter(x => ((x._4 >= 4) && (x._4 <= 6) && (x._2 >= 26) && (x._2 <= 35)))
-    val rdd003 = rdd002.map(x => (x._1, x._2 * x._3))
-    val rdd004 = sc.textFile(dbDir + "/ddate*").map(line => {
-      val columns = line.split("\\|")
-      val dateYearMonth = columns(6)
-      if (dateYearMonth == "Jan1994") {
-        (columns(0), columns(0))
+      val dateKey = columns(0).toInt
+      val yearMonth = columns(6)
+      if (yearMonth == "Jan1994") {
+        (dateKey, 0)
       } else {
         null
       }
     }).filter(_ != null)
-    val rdd009 = rdd003.join(rdd004).map(x => (1, x._2._1))
-    val rdd012 = rdd009.reduceByKey(_ + _)
-    (rdd012, "hand_opt_ssb_1_2")
+
+    val rddLo = sc.textFile(dbDir + "/lineorder*").map(line => {
+      val columns = line.split("\\|")
+      val oderDate = columns(5).toInt
+      val quantity = columns(8).toInt
+      val extendedPrice = columns(9).toFloat
+      val discount = columns(11).toInt
+      val revenue = extendedPrice * discount
+      (oderDate, (revenue, quantity, discount))
+    })
+
+    val rddLoFilter = rddLo.filter{
+      case (oderDate, (revenue, quantity, discount)) =>
+        (discount >= 4 ) && (discount <= 6) && (quantity >= 26) && (quantity <= 35)
+    }
+    val rddLoDate = rddLoFilter.join(rddDate).map{
+      case (oderDate, ((revenue, quantity, discount), zero)) =>
+        (1, revenue)
+    }
+    val rddAggregate = rddLoDate.reduceByKey(_ + _)
+    (rddAggregate, "hand_opt_ssb_1_2")
   }
 
   val ssb_1_2 = (sc: SparkContext, dbDir: String) => {
-
     val rdd001 = sc.textFile(dbDir + "/lineorder*").map(line => {
       val columns = line.split("\\|")
       (columns(0).toInt, columns(1).toInt, columns(2).toInt, columns(3).toInt, columns(4).toInt, columns(5), columns(6), columns(7), columns(8).toInt, columns(9).toFloat, columns(10).toFloat, columns(11).toInt, columns(12).toFloat, columns(13).toFloat, columns(14).toInt, columns(15), columns(16))
