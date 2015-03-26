3bc2ff16f7455dcbc0d1905d17caf417e5b8e93f
Meisam Fathi Salmi <meisam.fathi@gmail.com>
2015-03-26 17:54:55 -0400

    moving the fist filter criterion to a new line
    just for readability

--- src/main/scala/edu/osu/cse/fathi/spark/ssb/SsbQueryRunnerOnSpark.scala
+++ src/main/scala/edu/osu/cse/fathi/spark/ssb/SsbQueryRunnerOnSpark.scala
@@ -123,7 +123,8 @@ object SsbQueryRunnerOnSpark {
    * select sum(lo_extendedprice*lo_discount) as revenue
    * from lineorder,ddate
    * where lo_orderdate = d_datekey
-   * and d_year = 1993 and lo_discount>=1
+   * and d_year = 1993
+   * and lo_discount>=1
    * and lo_discount<=3
    * and lo_quantity<25
    */
